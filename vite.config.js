a94479ce7307e5b6deae6e22b4c6967a7aebde65
Brenner Endreson <brennerendreson@gmail.com>
2026-07-10 21:59:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Isso é necessário para o GitHub Pages encontrar seus arquivos se o repositório não for a página principal do seu perfil
-  base: '/tracker-organizer/', 
+  base: '/backlog/', 
 })
